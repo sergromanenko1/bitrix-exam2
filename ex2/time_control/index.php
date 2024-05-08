a6c7bc4de2d84645f6fb3c36364422c958c82e49
Sergey <sergromanenko1@gmail.com>
2024-05-08 17:37:02 +0300

    [ex2-10] Estimate the longest page and the longest component

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -2,4 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
 ?>Самая ресурсоемкая страница&nbsp;/products/index.php 40.14% в общей статистике нагрузки.<br>
- Уменьшенение объёма кеша при помещении только нужных данных 10кб - 7кб=3кб<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ Уменьшенение объёма кеша при помещении только нужных данных 10кб - 7кб=3кб<br>
+ Самый долгий компонент&nbsp;bitrix:catalog:&nbsp;<nobr>0.0262 с</nobr><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
