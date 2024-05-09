711bdaed229dae039b10abd64a2db6872cfeca0b
Sergey <sergromanenko1@gmail.com>
2024-05-09 07:23:03 +0300

    [ex2-11] Estimate the longest page and a component with the most requests

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -1,6 +1,10 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>Самая ресурсоемкая страница&nbsp;/products/index.php 40.14% в общей статистике нагрузки.<br>
- Уменьшенение объёма кеша при помещении только нужных данных 10кб - 7кб=3кб<br>
- Самый долгий компонент&nbsp;bitrix:catalog:&nbsp;<nobr>0.0262 с</nobr><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>Самая ресурсоемкая страница&nbsp;/products/index.php 22.35% в общей статистике нагрузки, среднее время выполнения&nbsp;0.2754 сек.<br>
+ <br>
+ Уменьшенение объёма кеша при помещении только нужных данных 10кб - 7кб=3кб.<br>
+ <br>
+ Самый долгий компонент&nbsp;bitrix:catalog:&nbsp;<nobr>0.0262 с.<br>
+ </nobr><br>
+ Больше всего запросов у компонента&nbsp;bitrix:news.list, всего 37 запросов.<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
